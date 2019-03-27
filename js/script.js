3ba898eb0cae1255f33b075e98e140948b4b2014
Ifeanyi Osinakayah Joseph <ifeanyiosinakayah15@gmail.com>
2019-03-27 12:41:36 +0100

    Created a weaher module and added functions to it

--- js/script.js
+++ js/script.js
@@ -1,12 +1,32 @@
-const fahrenheitToCelcius = (fahrenheit) => {
-  return (fahrenheit - 32) * (5/9)
-}
-const parseJsonResponse = (weatherData) => {
-  if (typeof weatherData === "string") weatherData = JSON.parse(weatherData)
-  const fahrenheit = weatherData.main.temp;
-  const celcius = fahrenheitToCelcius(fahrenheit)
-  return {
-    celcius,
-    fahrenheit
+
+const WeatherApp = (function () {
+  const fahrenheitToCelcius = (fahrenheit) => {
+    return (fahrenheit - 32) * (5/9)
   }
-}
+  const parseJsonResponse = (weatherData) => {
+    if (typeof weatherData === "string") weatherData = JSON.parse(weatherData)
+    const fahrenheit = weatherData.main.temp;
+    const celcius = fahrenheitToCelcius(fahrenheit)
+    return {
+      celcius,
+      fahrenheit
+    }
+  }
+  const fetchWeather = (city) => {
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=e686eb81691141fa6168377f12b326cd`
+    return new Promise((resolve, reject) => {
+      fetch(URL).then((response) => {
+        response.json().then((res) => {
+          resolve(parseJsonResponse(res))
+        }).catch((error) => {
+          reject(error)
+        })
+      }).catch((error) => {
+        reject(error)
+        console.log(error)
+      })
+    });
+
+  }
+
+})()
