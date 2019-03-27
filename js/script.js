2fbe244eabf512d61af5951db0fd079c9a8f1908
Sanel Hodzic <sanel_hodzic_tz_91@hotmail.com>
2019-03-27 12:34:34 +0100

    parseJsonResponse function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-
+const fahrenheitToCelcius = (fahrenheit) => {
+  return (fahrenheit - 32) * (5/9)
+}
+const parseJsonResponse = (weatherData) => {
+  if (typeof weatherData === "string") weatherData = JSON.parse(weatherData)
+  const fahrenheit = weatherData.main.temp;
+  const celcius = fahrenheitToCelcius(fahrenheit)
+  return {
+    celcius,
+    fahrenheit
+  }
+}
